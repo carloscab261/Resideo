94c4192772242270886e25557266948f732a165f
Carlos <carlos.cabrera.carrasco261@gmail.com>
2021-11-08 18:17:02 -0700

    se realizaron modificaciones y avances  ala parte de administrador y se paso a php

---
+++ Administrador/inventario.php
@@ -0,0 +1,177 @@
+<?php
+
+
+
+include("./Navbar.php");
+
+
+echo '
+<div class="container border p-2">
+<div class="row align-items-start">
+  <div class="col-9">
+    <a class="liga" href="#"><span class="badge bg-danger m-3">Modificar</span></a>
+    <a class="liga" href="#"><span class="badge bg-danger">Agregar nueva herramienta</span></a>
+  </div>
+  <div class="col-3 align-items-end">
+    <div class="input-group">
+      <div class="form-outline">
+        <input id="search-focus" type="search" id="form1" class="form-control p-3" />
+      </div>
+      <button  type="button" class="btn btn-primary btn-lg">
+        Buscar
+      </button>
+    </div>
+  </div>
+  
+</div>
+</div>
+
+
+<!--        Contenido          -->
+<div class="container">
+<div class="row align-items-center">
+
+  <div class="card m-3" style="width: 20rem;">
+    <img class="card-img-top" src="../img/Taladro.jpeg" style="width: 15rem; margin:auto;" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">01 Taladro</h5>
+        <p class="card-text">Taladro inalambrico black decker </p>
+        <span class="badge bg-success">Disponible</span></h1>
+        <!--        <span class="badge bg-danger">En uso</span></h1>          -->
+      </br>
+        <p class="card-text">En posesión de: </p>
+        <p class="nombre">carlos cabrera</p>
+        
+    </div>
+  </div>
+  <div class="card m-3" style="width: 20rem;">
+    <img class="card-img-top" src="../img/Taladro.jpeg" style="width: 15rem; margin:auto;" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">01 Taladro</h5>
+        <p class="card-text">Taladro inalambrico black decker </p>
+        <span class="badge bg-success">Disponible</span></h1>
+        <!--        <span class="badge bg-danger">En uso</span></h1>          -->
+        <p class="card-text">En posesión de: Carlos Cabrera</p>
+    </div>
+  </div>
+  <div class="card m-3" style="width: 20rem;">
+    <img class="card-img-top" src="../img/Taladro.jpeg" style="width: 15rem; margin:auto;" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">01 Taladro</h5>
+        <p class="card-text">Taladro inalambrico black decker </p>
+        <span class="badge bg-success">Disponible</span></h1>
+        <!--        <span class="badge bg-danger">En uso</span></h1>          -->
+        <p class="card-text">En posesión de: Carlos Cabrera</p>
+    </div>
+  </div>
+  <div class="card m-3" style="width: 20rem;">
+    <img class="card-img-top" src="../img/Taladro.jpeg" style="width: 15rem; margin:auto;" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">01 Taladro</h5>
+        <p class="card-text">Taladro inalambrico black decker </p>
+        <span class="badge bg-success">Disponible</span></h1>
+        <!--        <span class="badge bg-danger">En uso</span></h1>          -->
+        <p class="card-text">En posesión de: Carlos Cabrera</p>
+    </div>
+  </div>
+  <div class="card m-3" style="width: 20rem;">
+    <img class="card-img-top" src="../img/Taladro.jpeg" style="width: 15rem; margin:auto;" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">01 Taladro</h5>
+        <p class="card-text">Taladro inalambrico black decker </p>
+        <span class="badge bg-success">Disponible</span></h1>
+        <!--        <span class="badge bg-danger">En uso</span></h1>          -->
+        <p class="card-text">En posesión de: Carlos Cabrera</p>
+    </div>
+  </div>
+
+  <div class="card m-3" style="width: 20rem;">
+    <img class="card-img-top" src="../img/Taladro.jpeg" style="width: 15rem; margin:auto;" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">01 Taladro</h5>
+        <p class="card-text">Taladro inalambrico black decker </p>
+        <span class="badge bg-success">Disponible</span></h1>
+        <!--        <span class="badge bg-danger">En uso</span></h1>          -->
+        <p class="card-text">En posesión de: Carlos Cabrera</p>
+    </div>
+  </div>
+  <div class="card m-3" style="width: 20rem;">
+    <img class="card-img-top" src="../img/Taladro.jpeg" style="width: 15rem; margin:auto;" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">01 Taladro</h5>
+        <p class="card-text">Taladro inalambrico black decker </p>
+        <span class="badge bg-success">Disponible</span></h1>
+        <!--        <span class="badge bg-danger">En uso</span></h1>          -->
+        <p class="card-text">En posesión de: Carlos Cabrera</p>
+    </div>
+  </div>
+  <div class="card m-3" style="width: 20rem;">
+    <img class="card-img-top" src="../img/Taladro.jpeg" style="width: 15rem; margin:auto;" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">01 Taladro</h5>
+        <p class="card-text">Taladro inalambrico black decker </p>
+        <span class="badge bg-success">Disponible</span></h1>
+        <!--        <span class="badge bg-danger">En uso</span></h1>          -->
+        <p class="card-text">En posesión de: Carlos Cabrera</p>
+    </div>
+  </div>
+  <div class="card m-3" style="width: 20rem;">
+    <img class="card-img-top" src="../img/Taladro.jpeg" style="width: 15rem; margin:auto;" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">01 Taladro</h5>
+        <p class="card-text">Taladro inalambrico black decker </p>
+        <span class="badge bg-success">Disponible</span></h1>
+        <!--        <span class="badge bg-danger">En uso</span></h1>          -->
+        <p class="card-text">En posesión de: Carlos Cabrera</p>
+    </div>
+  </div>
+  <div class="card m-3" style="width: 20rem;">
+    <img class="card-img-top" src="../img/Taladro.jpeg" style="width: 15rem; margin:auto;" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">01 Taladro</h5>
+        <p class="card-text">Taladro inalambrico black decker </p>
+        <span class="badge bg-success">Disponible</span></h1>
+        <!--        <span class="badge bg-danger">En uso</span></h1>          -->
+        <p class="card-text">En posesión de: Carlos Cabrera</p>
+    </div>
+  </div>
+  <div class="card m-3" style="width: 20rem;">
+    <img class="card-img-top" src="../img/Taladro.jpeg" style="width: 15rem; margin:auto;" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">01 Taladro</h5>
+        <p class="card-text">Taladro inalambrico black decker </p>
+        <span class="badge bg-success">Disponible</span></h1>
+        <!--        <span class="badge bg-danger">En uso</span></h1>          -->
+        <p class="card-text">En posesión de: Carlos Cabrera</p>
+    </div>
+  </div>
+  <div class="card m-3" style="width: 20rem;">
+    <img class="card-img-top" src="../img/Taladro.jpeg" style="width: 15rem; margin:auto;" alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">01 Taladro</h5>
+        <p class="card-text">Taladro inalambrico black decker </p>
+        <span class="badge bg-success">Disponible</span></h1>
+        <!--        <span class="badge bg-danger">En uso</span></h1>          -->
+        <p class="card-text">En posesión de: Carlos Cabrera</p>
+    </div>
+  </div>
+  
+
+
+
+  
+
+</div>
+</div>
+
+
+
+</body>
+
+</html>
+
+';
+
+
+
+?>
